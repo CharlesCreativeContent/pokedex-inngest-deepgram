b379add0ed539f79501ef8fd4f0b51f6b63b6abe
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2025-08-22 17:57:38 -0700

    Add files via upload

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from 'next/server';
 import { inngest } from "../../../inngest/inngest"
 
+const BASE_URL = "https://api.inngest.com" // ??  "https://pokedex-inngest-deepgram.vercel.app"
+
 export async function POST(req: Request) {
   const { message } = await req.json();
 
@@ -9,6 +11,31 @@ export async function POST(req: Request) {
     name: 'test/hello.world',
     data: { message },
   });
+const id = res.ids[0]
 
-  return NextResponse.json({ eventId: res.ids[0] });
+  let runs = await triggerIngest(id);
+  console.log("currentSnag",runs)
+  while (runs[0].status !== "Completed") {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    
+    runs = await triggerIngest(id);
+    console.log("finalSnag",runs)
+
+    if (runs[0].status === "Failed" || runs[0].status === "Cancelled") {
+      throw new Error(`Function run ${runs[0].status}`);
+    }
+  }
+  return NextResponse.json({ eventId: id, generation: runs });
+
+async function triggerIngest(id: string){
+return await fetch(`${BASE_URL}/v1/events/${id}/runs`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`,
+      ...{ 'x-inngest-env': `${process.env.INNGEST_EVENT_KEY}` },
+    },
+  })
+  .then(inference=>inference.json())
+}
 }
